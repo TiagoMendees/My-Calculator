5791d0729e31acd5b2ccab8c1929396016a0cf04
TiagoMendes <85739128+TiagoMendees@users.noreply.github.com>
2023-05-06 16:48:40 +0100

    Version 1.1
    Main Changes:
    -UI Updated;
    -Solved Bugs.
    
    To do:
    -Upgrade Day Light UI;
    -Multiply and divide functions.
    
    Notes:
    -Multiply and divide function are not working.

--- app/src/main/java/com/nytrostudios/mycalculator/MainActivity.kt
+++ app/src/main/java/com/nytrostudios/mycalculator/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import android.widget.Button
 import com.nytrostudios.mycalculator.databinding.ActivityMainBinding
 
+fun concat(s1: String, s2: String): String {
+    return s1 + s2
+}
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -15,17 +19,45 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
-
         val equal = findViewById<Button>(R.id.Equals)
-        val op = "1+1"
+        val zero = findViewById<Button>(R.id.zero)
+        val one = findViewById<Button>(R.id.one)
+        val two = findViewById<Button>(R.id.two)
+        val three = findViewById<Button>(R.id.three)
+        val four = findViewById<Button>(R.id.four)
+        val five = findViewById<Button>(R.id.five)
+        val six = findViewById<Button>(R.id.six)
+        val seven = findViewById<Button>(R.id.seven)
+        val eight = findViewById<Button>(R.id.eight)
+        val nine = findViewById<Button>(R.id.nine)
+        val plus = findViewById<Button>(R.id.Plus)
+        val divide = findViewById<Button>(R.id.divide)
+        val minus = findViewById<Button>(R.id.minus)
+        val multiply = findViewById<Button>(R.id.Multiply)
+        val clear = findViewById<Button>(R.id.Clear)
+        var op = "1+1"
 
 
         equal.setOnClickListener {
-            binding.sampleText.text = calc(op)
+            binding.sampleText2.text = calc(op)
 
         }
+
+        clear.setOnClickListener {op = ""; binding.sampleText.text = ""; binding.sampleText2.text = ""}
+        plus.setOnClickListener { op = concat(op, "+"); binding.sampleText.text = op}
+        divide.setOnClickListener { op = concat(op, "/"); binding.sampleText.text = op }
+        multiply.setOnClickListener { op = concat(op, "*"); binding.sampleText.text = op }
+        minus.setOnClickListener { op = concat(op, "-"); binding.sampleText.text = op }
+        one.setOnClickListener { op = concat(op, "1"); binding.sampleText.text = op }
+        two.setOnClickListener { op = concat(op, "2"); binding.sampleText.text = op }
+        three.setOnClickListener { op = concat(op, "3"); binding.sampleText.text = op }
+        four.setOnClickListener { op = concat(op, "4"); binding.sampleText.text = op }
+        five.setOnClickListener { op = concat(op, "5"); binding.sampleText.text = op }
+        six.setOnClickListener { op = concat(op, "6"); binding.sampleText.text = op }
+        seven.setOnClickListener { op = concat(op, "7"); binding.sampleText.text = op }
+        eight.setOnClickListener { op = concat(op, "8"); binding.sampleText.text = op }
+        nine.setOnClickListener { op = concat(op, "9"); binding.sampleText.text = op }
+        zero.setOnClickListener { op = concat(op, "0"); binding.sampleText.text = op }
     }
 
     /**
